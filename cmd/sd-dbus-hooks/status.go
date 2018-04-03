45d0607e564f772baeb6ec7584344b09e47cee7a
Pavel Podkorytov <pod.pavel@gmail.com>
2018-04-03 12:06:53 +0500

    Revert unit name checking in /unit/status

--- cmd/sd-dbus-hooks/status.go
+++ cmd/sd-dbus-hooks/status.go
@@ -17,12 +17,12 @@ func (h unitStatusHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Path[len("/unit/status/"):]
 
 	// check if unit in config
-	// unitCfg, err := h.cfg.getUnit(name)
-	// if err != nil {
-	// 	log.Printf("[ERROR] %s", err)
-	// 	w.WriteHeader(http.StatusForbidden)
-	// 	return
-	// }
+	_, err := h.cfg.getUnit(name)
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
 
 	log.Printf("[INFO] get unit status %v", name)
 
@@ -43,20 +43,4 @@ func (h unitStatusHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-
-	// for _, unit := range units {
-	// 	if unit.Name == name {
-	// 		fmt.Fprintf(w, "Name         %v\n", unit.Name)
-	// 		fmt.Fprintf(w, "Description  %v\n", unit.Description)
-	// 		fmt.Fprintf(w, "LoadState    %v\n", unit.LoadState)
-	// 		fmt.Fprintf(w, "ActiveState  %v\n", unit.ActiveState)
-	// 		fmt.Fprintf(w, "SubState     %v\n", unit.ActiveState)
-	// 		fmt.Fprintf(w, "BlockedBy    %v\n", unitCfg.BlockedBy)
-	// 		return
-	// 	}
-	// }
-
-	// log.Printf("[ERROR] unit %v not found", name)
-	// w.WriteHeader(http.StatusBadRequest)
-	// return
 }
