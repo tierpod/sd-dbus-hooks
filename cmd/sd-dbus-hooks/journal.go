f5de26369cf6a59d9e692f7ef748f153aaebaf05
Pavel Podkorytov <pod.pavel@gmail.com>
2018-04-03 10:47:48 +0500

    Add /journal/ entrypoint

---
+++ cmd/sd-dbus-hooks/journal.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/coreos/go-systemd/dbus"
+	"github.com/coreos/go-systemd/sdjournal"
+)
+
+const journalNumEntries = 20
+
+type journalHandler struct {
+	conn *dbus.Conn
+	cfg  *Config
+}
+
+func (h journalHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path[len("/journal/"):]
+
+	jcfg := sdjournal.JournalReaderConfig{
+		NumFromTail: 20,
+		Matches: []sdjournal.Match{
+			{
+				Field: sdjournal.SD_JOURNAL_FIELD_SYSTEMD_UNIT,
+				Value: name,
+			},
+		},
+	}
+
+	jr, err := sdjournal.NewJournalReader(jcfg)
+	if err != nil {
+		log.Printf("[ERROR] journal: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("[INFO] journal: show last %v entries for %v", journalNumEntries, name)
+	scanner := bufio.NewScanner(jr)
+	for scanner.Scan() {
+		fmt.Fprintf(w, "%v\n", scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("[ERROR] journal: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
--- cmd/sd-dbus-hooks/main.go
+++ cmd/sd-dbus-hooks/main.go
@@ -50,6 +50,7 @@ func main() {
 	http.Handle("/unit/start/", unitStartHandler{conn, cfg})
 	http.Handle("/unit/stop/", unitStopHandler{conn, cfg})
 	http.Handle("/unit/status/", unitStatusHandler{conn, cfg})
+	http.Handle("/journal/", journalHandler{conn, cfg})
 
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
