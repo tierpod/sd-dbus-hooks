09ff28d549feb947545fbecd5509beaf3a6ed744
Pavel Podkorytov <pod.pavel@gmail.com>
2018-04-04 15:17:29 +0500

    Fixed empty x_token setting

--- cmd/sd-dbus-hooks/token.go
+++ cmd/sd-dbus-hooks/token.go
@@ -12,14 +12,15 @@ type tokenStore struct {
 func (t *tokenStore) middleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("X-Token")
-		if t.token == "" || t.token != token {
-			log.Printf("[ERROR] xtoken: forbidden request from %v to %v: wrong X-Token header: \"%v\"", r.RemoteAddr, r.URL.Path, token)
-			http.Error(w, "forbidden", http.StatusForbidden)
+		if t.token == "" || t.token == token {
+			log.Printf("[INFO] xtoken: accept request from %v to %v: X-Token \"%v\"", r.RemoteAddr, r.URL.Path, token)
+			next.ServeHTTP(w, r)
 			return
 		}
 
-		log.Printf("[INFO] xtoken: accept request from %v to %v: X-Token \"%v\"", r.RemoteAddr, r.URL.Path, token)
-		next.ServeHTTP(w, r)
+		log.Printf("[ERROR] xtoken: forbidden request from %v to %v: wrong X-Token header: \"%v\"", r.RemoteAddr, r.URL.Path, token)
+		http.Error(w, "forbidden", http.StatusForbidden)
+		return
 	}
 
 	return http.HandlerFunc(fn)
