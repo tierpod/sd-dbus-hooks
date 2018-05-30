4f28313d4189c29a096f3c15eee99cd8cea868e9
Pavel Podkorytov <pod.pavel@gmail.com>
2018-05-30 08:15:47 +0500

    vuejs: move from bootstrap+jquery to bulma

--- webui/js/app.js
+++ webui/js/app.js
@@ -10,15 +10,24 @@ const HTTP = axios.create({
 // header component
 Vue.component('navbar', {
   props: ['count'],
-  template: `<nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">sd-dbus-hooks ({{ count }})</a>
-    <button class="btn btn-primary" type="button" v-on:click="$emit('get-units')">reload</button>
-  </nav>`,
+  template: `
+<nav class="navbar is-dark">
+  <div class="navbar-brand">
+    <a class="navbar-item">sd-dbus-hooks ({{ count }})</a>
+  </div>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="button is-success" v-on:click="$emit('get-units')">reload</a>
+      </div>
+    </div>
+  </div>
+</nav>`,
 });
 
 Vue.component('unit-start-button', {
   props: ['name'],
-  template: `<button type="button" class="btn btn-sm btn-primary" v-bind:name="name" v-on:click="start">start</button>`,
+  template: `<a class="button is-primary" v-bind:name="name" v-on:click="start">start</a>`,
 
   methods: {
     start: function() {
@@ -38,7 +47,7 @@ Vue.component('unit-start-button', {
 
 Vue.component('unit-stop-button', {
   props: ['name'],
-  template: `<button type="button" class="btn btn-sm btn-danger" v-bind:name="name" v-on:click="stop">stop</button>`,
+  template: `<a class="button is-danger" v-bind:name="name" v-on:click="stop">stop</a>`,
 
   methods: {
     stop: function() {
@@ -58,7 +67,7 @@ Vue.component('unit-stop-button', {
 
 Vue.component('unit-journal-button', {
   props: ['name'],
-  template: `<button type="button" class="btn btn-sm btn-info" v-bind:name="name" v-on:click="showModal">journal</button>`,
+  template: `<a class="button is-info" v-bind:name="name" v-on:click="showModal">journal</a>`,
 
   methods: {
     getData: function() {
@@ -80,7 +89,7 @@ Vue.component('unit-journal-button', {
     showModal: function() {
       var self = this;
       self.getData();
-      $("#journal-modal").modal("show");
+      app.showJournal = true;
     },
   }
 });
@@ -90,36 +99,51 @@ Vue.component('unit-item', {
   props: ['unit'],
   template: `
 <tr>
-  <td v-bind:title="unit.Description">{{ unit.Name }}</td>
-  <td><span class="badge" v-bind:class="badgeClass">{{ unit.ActiveState }} / {{ unit.SubState }}</span></td>
   <td>
-    <unit-start-button v-if="!isUnitActive" v-bind:name="unit.Name"></unit-start-button>
-    <unit-stop-button v-else v-bind:name="unit.Name"></unit-stop-button>
-    <unit-journal-button v-bind:name="unit.Name"></unit-journal-button>
+    {{ title }} <span class="tag" v-bind:class="tagClass">{{ unit.ActiveState }} / {{ unit.SubState }}</span>
+  </td>
+  <td>
+    <div class="field has-addons">
+      <p class="control">
+        <unit-start-button v-if="!isUnitActive" v-bind:name="unit.Name"></unit-start-button>
+        <unit-stop-button v-else v-bind:name="unit.Name"></unit-stop-button>
+      </p>
+      <p class="control">
+        <unit-journal-button v-bind:name="unit.Name"></unit-journal-button>
+      </p>
+    </div>
   </td>
 </tr>`,
 
   computed: {
-    badgeClass: function() {
+    tagClass: function() {
       switch(this.unit.ActiveState) {
         case "active":
-          return 'badge-success';
+          return 'is-success';
           break
         case "inactive":
-          return 'badge-secondary';
+          return 'is-light';
           break
         case "failed":
-          return 'badge-danger';
+          return 'is-danger';
           break
         default:
-          return 'badge-warning';
+          return 'is-warning';
           break
       };
     },
 
     isUnitActive: function() {
       return this.unit.ActiveState === 'active';
-    }
+    },
+
+    title: function() {
+      if (this.unit.Description) {
+        return `${ this.unit.Name } (${ this.unit.Description })`;
+      } else {
+        return this.unit.Name;
+      };
+    },
   }
 });
 
@@ -127,26 +151,21 @@ Vue.component('unit-item', {
 Vue.component('journal-modal', {
   props: ['item'],
   template: `
-  <div class="modal fade" id="journal-modal" role="dialog" aria-labelledby="JournalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">journal for: {{ item.name }}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" v-on:click="hide">
-              <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-          <pre>
-{{ item.data }}
-          </pre>
-        </div>
-      </div>
-    </div>
-  </div>`,
+<div class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">journal for {{ item.name }}</p>
+      <button class="delete" aria-label="close" v-on:click="hide"></button>
+    </header>
+    <section class="modal-card-body">
+      <pre>{{ item.data }}</pre>
+    </section>
+  </div>
+</div>`,
 
   methods: {
-    hide: function() { $("#journal-modal").modal("hide") }
+    hide: function() { app.showJournal = false; }
   }
 });
 
@@ -154,31 +173,25 @@ Vue.component('journal-modal', {
 Vue.component('units-table', {
   props: ['units'],
   template: `
-<div class="container">
-  <div class="row">
-      <div class="col">
-          <table class="table table-striped" v-if="units.length">
-              <thead>
-                  <tr>
-                  <th scope="col">unit name</th>
-                  <th scope="col">status</th>
-                  <th scope="col">actions</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr is="unit-item" v-for="item in units" v-bind:unit="item"></tr>
-              </tbody>
-          </table>
-      </div>
-  </div>
-</div>`,
-})
+<table class="table is-fullwidth">
+  <thead>
+    <tr>
+      <th>unit</th>
+      <th>actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr is="unit-item" v-for="item in units" v-bind:unit="item"></tr>
+  </tbody>
+</table>`,
+});
 
 var app = new Vue({
   el: '#app',
 
   data: {
     units: [],
+    showJournal: false,
     journalItem: {
       name: "",
       data: "",
